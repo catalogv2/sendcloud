4f2c9f05c5818da954d86f1f3f539544edca8b83
Marco Jansen <m.c.jansen@gmail.com>
2022-03-28 13:30:12 +0200

    V

---
+++ lib/sendcloud/objects/shipping_product.rb
@@ -0,0 +1,4 @@
+module Sendcloud
+  class ShippingProduct < Object
+  end
+end
--- lib/sendcloud/resources/shipping_product.rb
+++ lib/sendcloud/resources/shipping_product.rb
@@ -3,7 +3,7 @@ module Sendcloud
     def list(**params)
       response = get_request("shipping-products", params: params)
       puts response.inspect
-      Collection.from_response(response, key: "shipping-products", type: ShippingMethod)
+      Collection.from_response(response, key: nil, type: ShippingProduct)
     end
   end
 end
